b75e49d25599bcd9dfeff99ebfb2496e991881bc
Matt Thankachan <mathews.thankachan@gmail.com>
2020-03-15 20:17:21 +0000

    updated

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,23 +29,38 @@
         private void InitializeComponent()
         {
             this.btnBulkInsert = new System.Windows.Forms.Button();
+            this.btnBlobStorageBulkInsert = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnBulkInsert
             // 
-            this.btnBulkInsert.Location = new System.Drawing.Point(371, 158);
+            this.btnBulkInsert.Location = new System.Drawing.Point(214, 158);
             this.btnBulkInsert.Name = "btnBulkInsert";
             this.btnBulkInsert.Size = new System.Drawing.Size(410, 127);
             this.btnBulkInsert.TabIndex = 0;
-            this.btnBulkInsert.Text = "BulkInsert";
+            this.btnBulkInsert.Text = "BulkInsert - Local";
             this.btnBulkInsert.UseVisualStyleBackColor = true;
             this.btnBulkInsert.Click += new System.EventHandler(this.btnBulkInsert_Click);
             // 
+            // btnBlobStorageBulkInsert
+            // 
+            this.btnBlobStorageBulkInsert.Location = new System.Drawing.Point(750, 158);
+            this.btnBlobStorageBulkInsert.Name = "btnBlobStorageBulkInsert";
+            this.btnBlobStorageBulkInsert.Size = new System.Drawing.Size(410, 127);
+            this.btnBlobStorageBulkInsert.TabIndex = 1;
+            this.btnBlobStorageBulkInsert.Text = "BulkInsert-Blob Storage";
+            this.btnBlobStorageBulkInsert.UseVisualStyleBackColor = true;
+            this.btnBlobStorageBulkInsert.Click += new System.EventHandler(this.btnBlobStorageBulkInsert_Click);
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(1209, 437);
+            this.ClientSize = new System.Drawing.Size(1559, 517);
+            this.Controls.Add(this.btnBlobStorageBulkInsert);
             this.Controls.Add(this.btnBulkInsert);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -55,6 +70,7 @@
 
 
         private System.Windows.Forms.Button btnBulkInsert;
+        private System.Windows.Forms.Button btnBlobStorageBulkInsert;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,11 +8,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Container = Microsoft.Azure.Cosmos.Container;
 using Microsoft.Azure.Cosmos;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Auth;
 
 namespace BulkInsert
 {
     public partial class Form1 : Form
     {
+        string EndpointUrl = ConfigurationManager.AppSettings["EndpointUrl"];
+        string AuthorizationKey = ConfigurationManager.AppSettings["AuthorizationKey"];
+        string DatabaseName = ConfigurationManager.AppSettings["DatabaseName"];
+        string ContainerName = ConfigurationManager.AppSettings["ContainerName"];
         public Form1()
         {
             InitializeComponent();
@@ -26,32 +33,6 @@ namespace BulkInsert
         private async void btnBulkInsert_Click(object sender, EventArgs e)
         {
             btnBulkInsert.Enabled = false;
-            string EndpointUrl = ConfigurationManager.AppSettings["EndpointUrl"];
-            string AuthorizationKey = ConfigurationManager.AppSettings["AuthorizationKey"];
-            string DatabaseName = ConfigurationManager.AppSettings["DatabaseName"];
-            string ContainerName = ConfigurationManager.AppSettings["ContainerName"];
-
-            //Define the conectivity option to cosmos client
-            CosmosClientOptions options = new CosmosClientOptions()
-            {
-                AllowBulkExecution = true,
-                ConnectionMode = Microsoft.Azure.Cosmos.ConnectionMode.Direct,
-                MaxRequestsPerTcpConnection = 1000,
-                MaxRetryAttemptsOnRateLimitedRequests = 100,
-                RequestTimeout = new TimeSpan(0, 5, 0),
-                MaxTcpConnectionsPerEndpoint = 1000,
-                OpenTcpConnectionTimeout = new TimeSpan(0, 5, 0),
-            };
-          
-            #region CosmosDB Connection settings
-
-            CosmosClient client = new CosmosClient(EndpointUrl, AuthorizationKey, options);
-            Database database = await client.CreateDatabaseIfNotExistsAsync(DatabaseName);
-            Container container = await database.CreateContainerIfNotExistsAsync(ContainerName, "/Postcode");
-
-            #endregion
-
-
             string filePath = Path.GetDirectoryName(Application.ExecutablePath).Replace(@"bin\Debug", ConfigurationManager.AppSettings["FilePath"]);
 
             List<PricePaidData> lstPricedata = new List<PricePaidData>();
@@ -70,22 +51,128 @@ namespace BulkInsert
             List<PricePaidData> lstPricedata1 = lstPricedata.GetRange(0, 500000);
 
 
-            sw.Start();
+            sw.Start();// initialize the stop watch for testing the throughput of Cosmosdb SQL API
+
+
+            BulkInsertCreateAsyncMethod(lstPricedata1);
+
+
+
+            btnBulkInsert.Enabled = true;
+            Console.WriteLine("Total Time writing to CosmosDB  :  " + (sw.ElapsedMilliseconds / 1000).ToString() + "  seconds");
+            //MessageBox.Show("Total Time writing to CosmosDB  :  " + (sw.ElapsedMilliseconds / 1000).ToString() + "  seconds");
+            sw.Stop();
+
+
+        }
+
+        //Method for Bulk insert records using createAsync method
+        public async void BulkInsertCreateAsyncMethod(List<PricePaidData> lstPricedata)
+        {
+
+            //Define the conectivity option to cosmos client
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                AllowBulkExecution = true,
+                ConnectionMode = ConnectionMode.Direct,
+                MaxRequestsPerTcpConnection = -1,
+                MaxTcpConnectionsPerEndpoint = -1,
+                ConsistencyLevel = ConsistencyLevel.Eventual,
+                MaxRetryAttemptsOnRateLimitedRequests = 999,
+                MaxRetryWaitTimeOnRateLimitedRequests = TimeSpan.FromHours(1),
+            };
+
+            #region CosmosDB Connection settings
+
+            CosmosClient client = new CosmosClient(EndpointUrl, AuthorizationKey, options);
+            Database database = await client.CreateDatabaseIfNotExistsAsync(DatabaseName);
+            //Container container = await database.CreateContainerIfNotExistsAsync(ContainerName, "/Postcode");
+            Container container = await database.DefineContainer(ContainerName, "/Postcode").WithIndexingPolicy()
+                .WithIndexingMode(IndexingMode.Consistent).WithIncludedPaths().Attach()
+                .WithExcludedPaths().Path("/*").Attach().Attach()
+                .CreateIfNotExistsAsync(5000);
+            
+
+            //database.ReplaceThroughputAsync()
+
+            //ThroughputResponse throughput = await container.ReplaceThroughputAsync(20000);
+            #endregion
             int cnt = 0;
-
-
-            #region CosmosDb async method
-
             List<Task> tasks = new List<Task>();
-            foreach (var item in lstPricedata1)
+
+
+
+            foreach (var item in lstPricedata)
             {
                 cnt++; // only used for debugging to see current record index being processed
                 tasks.Add(container.CreateItemAsync<PricePaidData>(item, new PartitionKey(item.Postcode)));
             }
 
             await Task.WhenAll(tasks);
-            #endregion
 
+            //throughput = await container.ReplaceThroughputAsync(400);
+        }
+
+
+
+
+        private void btnBlobStorageBulkInsert_Click(object sender, EventArgs e)
+        {
+            btnBlobStorageBulkInsert.Enabled = false;
+
+            string StorageEndpointUrl = ConfigurationManager.AppSettings["StorageEndpointUrl"];
+            string StorageAccountName = ConfigurationManager.AppSettings["StorageAccountName"];
+            string StorageAccountKey = ConfigurationManager.AppSettings["StorageAccountKey"];
+            string StorageContainerName = ConfigurationManager.AppSettings["StorageContainerName"];
+
+            StorageCredentials storageCredentials = new StorageCredentials(StorageAccountName, StorageAccountKey);
+
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+            CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(StorageContainerName);
+            CloudBlob cloudBlob = cloudBlobContainer.GetBlobReference("2019_PricePaidData.txt");
+
+
+            List<PricePaidData> lstPricedata = new List<PricePaidData>();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            using (var stream = cloudBlob.OpenRead())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+
+                        lstPricedata.Add(PopulatePriceData(line));
+                    }
+                }
+            }
+
+            //reading file from local to azure takes approx 53 seconds
+            //MessageBox.Show("Time Taken to read the file :" + (sw.ElapsedMilliseconds / 1000).ToString() + "\n Number of line : " + lstPricedata.Count.ToString());
+
+            sw.Restart(); //restarting the stop watch to calculate the time taken for bulk insert record only
+
+            //Create a new collection to test various range of data. This test code and needs to be removed
+            //List<PricePaidData> lstPricedata1 = lstPricedata.GetRange(0, lstPricedata.Count);
+            List<PricePaidData> lstPricedata1 = lstPricedata.GetRange(0, 5000);
+
+
+            BulkInsertCreateAsyncMethod(lstPricedata1);
+
+
+            btnBlobStorageBulkInsert.Enabled = true;
+            Console.WriteLine("Total Time writing to CosmosDB  :  " + (sw.ElapsedMilliseconds / 1000).ToString() + "  seconds");
+            //MessageBox.Show("Total Time writing to CosmosDB  :  " + (sw.ElapsedMilliseconds / 1000).ToString() + "  seconds");
+            sw.Stop();
+
+        }
+
+
+
+        public void BulkInsertCreateAsyncStreamWriterMethod(List<PricePaidData> lstPricedata)
+        {
             #region CosmosDb StreamWriter --> better performance
             //foreach (var item in lstPricedata1)
             //{
@@ -110,14 +197,8 @@ namespace BulkInsert
 
             //    }
             //}
+
             #endregion
-
-            btnBulkInsert.Enabled = true;
-            Console.WriteLine("Total Time writing to CosmosDB  :  " + (sw.ElapsedMilliseconds/1000).ToString() + "  seconds");
-            MessageBox.Show("Total Time writing to CosmosDB  :  " + (sw.ElapsedMilliseconds / 1000).ToString() + "  secods");
-            sw.Stop();
-
-
         }
 
 
@@ -133,33 +214,40 @@ namespace BulkInsert
                         string line;
                         while ((line = sr.ReadLine()) != null)
                         {
-                            string[] pricedataArray = line.Split(',');
-                            PricePaidData ppd = new PricePaidData()
-                            {
-                                Transaction_unique_identifieroperty = pricedataArray[0],
-                                Price = pricedataArray[1].Replace("\"", ""),
-                                Date_of_Transfer = pricedataArray[2].Replace("\"", ""),
-                                Postcode = pricedataArray[3].Replace("\"", ""),
-                                PropertyType = pricedataArray[4].Replace("\"", ""),
-                                isNew = pricedataArray[5].Replace("\"", ""),
-                                Duration = pricedataArray[6].Replace("\"", ""),
-                                PAON = pricedataArray[7].Replace("\"", ""),
-                                SAON = pricedataArray[8].Replace("\"", ""),
-                                Street = pricedataArray[9].Replace("\"", ""),
-                                Locality = pricedataArray[10].Replace("\"", ""),
-                                Town_City = pricedataArray[11].Replace("\"", ""),
-                                District = pricedataArray[12].Replace("\"", ""),
-                                County = pricedataArray[13].Replace("\"", ""),
-                                PPD_Category = pricedataArray[14].Replace("\"", ""),
-                                Record_Status = pricedataArray[15].Replace("\"", "")
 
-                            };
-                            lstPricedata.Add(ppd);
+                            lstPricedata.Add(PopulatePriceData(line));
                         }
                     }
                 }
             }
             return lstPricedata;
+        }
+
+        private PricePaidData PopulatePriceData(string line)
+        {
+
+            string[] pricedataArray = line.Split(',');
+            PricePaidData ppd = new PricePaidData()
+            {
+                Transaction_unique_identifieroperty = pricedataArray[0],
+                Price = pricedataArray[1].Replace("\"", ""),
+                Date_of_Transfer = pricedataArray[2].Replace("\"", ""),
+                Postcode = pricedataArray[3].Replace("\"", ""),
+                PropertyType = pricedataArray[4].Replace("\"", ""),
+                isNew = pricedataArray[5].Replace("\"", ""),
+                Duration = pricedataArray[6].Replace("\"", ""),
+                PAON = pricedataArray[7].Replace("\"", ""),
+                SAON = pricedataArray[8].Replace("\"", ""),
+                Street = pricedataArray[9].Replace("\"", ""),
+                Locality = pricedataArray[10].Replace("\"", ""),
+                Town_City = pricedataArray[11].Replace("\"", ""),
+                District = pricedataArray[12].Replace("\"", ""),
+                County = pricedataArray[13].Replace("\"", ""),
+                PPD_Category = pricedataArray[14].Replace("\"", ""),
+                Record_Status = pricedataArray[15].Replace("\"", "")
+
+            };
+            return ppd;
         }
     }
 }
